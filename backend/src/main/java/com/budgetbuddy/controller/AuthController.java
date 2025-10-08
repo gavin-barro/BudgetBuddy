4151dad4d14209d608152657ecf5b1d2c9392ce5
Gavin Barro <g.ab28320@gmail.com>
2025-10-08 19:04:24 -0400

    fix some path issues

--- backend/src/main/java/com/budgetbuddy/controller/AuthController.java
+++ backend/src/main/java/com/budgetbuddy/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.budgetbuddy.dto.UserDTO;
 // import com.budgetbuddy.entity.UserEntity;
 // import com.budgetbuddy.repository.UserRepository;
 import com.budgetbuddy.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    @Autowired
-    private AuthService authService;
+    private final AuthService authService;
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
--- backend/src/main/java/com/budgetbuddy/service/AuthService.java
+++ backend/src/main/java/com/budgetbuddy/service/AuthService.java
@@ -3,21 +3,21 @@ package com.budgetbuddy.service;
 import com.budgetbuddy.dto.UserDTO;
 import com.budgetbuddy.entity.UserEntity;
 import com.budgetbuddy.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    public AuthService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public UserEntity registerUser(UserDTO userDTO) {
-        // Basic validation
         if (userDTO.getFirstName() == null || userDTO.getFirstName().isEmpty()) {
             throw new IllegalArgumentException("First name is required");
         }
@@ -34,14 +34,12 @@ public class AuthService {
             throw new IllegalArgumentException("Email is already in use");
         }
 
-        // Create user entity
         UserEntity user = new UserEntity();
         user.setFirstName(userDTO.getFirstName());
         user.setLastName(userDTO.getLastName());
         user.setEmail(userDTO.getEmail());
         user.setPasswordHash(passwordEncoder.encode(userDTO.getPassword()));
 
-        // Save user to database
         return userRepository.save(user);
     }
 }
